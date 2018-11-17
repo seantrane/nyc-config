f1af3af66a2929702ae502cc456a78864b4d1770
Sean Trane <seantrane@users.noreply.github.com>
2018-11-17 08:26:50 -0500

    ci(release): update semantic-release-monorepo config

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,19 @@
+/**
+ * Release configuration for the monorepo
+ *
+ * Workaround to handle `semantic-release-monorepo` multipublishing.
+ *
+ * Resolve problems:
+ * 1. `semantic-release` does not support lerna-based repos aka monorepos out of box.
+ * 2. `semantic-release-monorepo` can not release several packages at once:
+ *    after the first sub-release it appends a new git version tag,
+ *    so any next run finds no changes.
+ *
+ * For more info;
+ * - https://github.com/qiwi/semantic-release-monorepo-hooks
+ * - https://github.com/Updater/semantic-release-monorepo
+ * - https://github.com/lerna/lerna
+ */
 const hooks = require('semantic-release-monorepo-hooks');
 const output = hooks();
 
@@ -7,6 +23,7 @@ const publish = output.isLastModified
       path: '@semantic-release/exec',
       cmd: 'echo "Execute publish/deploy commands and scripts"'
     },
+    '@semantic-release/npm',
     '@semantic-release/github'
   ]
   : [
@@ -34,9 +51,9 @@ module.exports = {
   verifyConditions: [],
   verifyRelease: [
     '@semantic-release/changelog',
-    '@semantic-release/npm',
     '@semantic-release/git',
-    '@semantic-release/github'
+    '@semantic-release/github',
+    '@semantic-release/npm'
   ]
     .map(require)
     .map(x => x.verifyConditions),
@@ -46,14 +63,24 @@ module.exports = {
       changelogTitle: '# CHANGELOG'
     },
     '@semantic-release/npm',
-    '@semantic-release/git'
+    {
+      'path': '@semantic-release/git',
+      'message': 'chore(' + output.package + '): release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }
   ],
   publish: publish,
-  success: [
-    '@semantic-release/github'
-  ],
-  fail: [
-    '@semantic-release/github'
-  ],
-  npmPublish: false
+  // success: [
+  //   '@semantic-release/github'
+  // ],
+  // fail: [
+  //   '@semantic-release/github'
+  // ],
+  monorepo: {
+    analyzeCommits: [
+      '@semantic-release/commit-analyzer'
+    ],
+    generateNotes: [
+      '@semantic-release/release-notes-generator'
+    ]
+  }
 };

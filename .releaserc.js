a092d552c5d1a3191b0901b06d4a1f294ee21d1f
Sean Trane <seantrane@users.noreply.github.com>
2018-11-18 18:24:20 -0500

    ci(release): clean up config file

--- .releaserc.js
+++ .releaserc.js
@@ -1,35 +1,15 @@
 /**
  * Release configuration for the monorepo
  *
- * Workaround to handle `semantic-release-monorepo` multipublishing.
- *
- * Resolve problems:
- * 1. `semantic-release` does not support lerna-based repos aka monorepos out of box.
- * 2. `semantic-release-monorepo` can not release several packages at once:
- *    after the first sub-release it appends a new git version tag,
- *    so any next run finds no changes.
- *
  * For more info;
- * - https://github.com/qiwi/semantic-release-monorepo-hooks
  * - https://github.com/Updater/semantic-release-monorepo
+ * - https://github.com/semantic-release/semantic-release
  * - https://github.com/lerna/lerna
  */
-// const hooks = require('semantic-release-monorepo-hooks');
-// const output = hooks();
-
-const publish = ['@semantic-release/github'];
-// if (output.isLastModified)
-  publish.unshift([
-    // {
-    //   path: '@semantic-release/exec',
-    //   cmd: 'echo "Execute publish/deploy commands and scripts"'
-    // },
-    '@semantic-release/npm'
-  ]);
 
 module.exports = {
   branch: 'master',
-  // tagFormat: 'v${version}',
+  // tagFormat: '${LERNA_PACKAGE_NAME}-v${version}',
   monorepo: {
     analyzeCommits: [
       '@semantic-release/commit-analyzer'
@@ -39,19 +19,9 @@ module.exports = {
     ]
   },
   /**
-   * Reduce expensive network calls (50%+ runtime reduction)
-   * By default, semantic-release's verifyConditions plugin configuration contains
-   * @semantic-release/npm and @semantic-release/github. These two plugins each make
-   * a network call to verify that credentials for the respective services are properly
-   * configured. When running in a monorepo, these verifications will be redundantly
-   * repeated for each and every package, greatly contributing to overall runtime.
-   * Optimally, we'd only want make these verification calls one time.
-   *
-   * By moving these plugins to the verifyRelease configuration, they will only run
-   * if semantic-release determines a release is to be made for a given package
-   * (at a time when the given verifications are actually relevant).
-   * Likely, most times semantic-release is run over a monorepo, only a small subset
-   * of all packages trigger releases.
+   * Move plugins from verifyConditions to verifyRelease to
+   * reduce expensive network calls (50%+ runtime reduction).
+   * https://github.com/Updater/semantic-release-monorepo#reduce-expensive-network-calls-50-runtime-reduction
    */
   verifyConditions: [],
   verifyRelease: [
@@ -73,7 +43,14 @@ module.exports = {
       'message': 'chore(${LERNA_PACKAGE_NAME}): release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }
   ],
-  publish: publish,
+  publish: [
+    // {
+    //   path: '@semantic-release/exec',
+    //   cmd: 'echo "Execute publish/deploy commands and scripts"'
+    // },
+    '@semantic-release/npm',
+    '@semantic-release/github'
+  ],
   success: [
     '@semantic-release/github'
   ],
